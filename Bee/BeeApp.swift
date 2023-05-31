36f27bc41c2689e6e0309ddc722867214fea89d5
Akshat Rastogi <akshatrastogi03@outlook.com>
2023-05-31 12:27:32 -0400

    Initial Commit

---
+++ Bee/BeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeApp.swift
+//  Bee
+//
+//  Created by Akshat Rastogi on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
